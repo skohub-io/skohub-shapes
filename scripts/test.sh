df17eab845c0c3ff324c04a7c37b7aaf785f3efb
@s.roertgen <sroertgen@gmail.com>
2023-01-25 14:48:16 +0100

    add github action for tests

---
+++ scripts/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+error=false
+# for files in tests/valid no output should be given
+FILES="$(pwd)/tests/valid/*"
+for f in $FILES
+do
+  echo "Processing $f ..."
+  # take action on each file. $f store current file name
+  scripts/validate-skos.sh -f "$f" -s skos.shacl.ttl -l both
+  if test $? -eq 1; then echo "Error: This should be valid, but is invalid"; error=true; fi;
+done
+
+# for files in tests/invalid output should be received
+FILES="$(pwd)/tests/invalid/*"
+for f in $FILES
+do
+  echo "Processing $f ..."
+  # validate with script and log error messages to dev null
+  scripts/validate-skos.sh -f "$f" -s skos.shacl.ttl -l both 2>/dev/null
+  if test $? -eq 0; then echo "Error: This should be invalid, but is valid"; error=true; fi
+done
+
+if test "$error" = true; 
+then 
+  echo "There were errors in your tests!" && rm $(pwd)/result.ttl ; exit 1
+else
+  rm $(pwd)/result.ttl
+fi

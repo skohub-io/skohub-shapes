49f2c4545ea605017f839638e32afb10e169e2d0
@s.roertgen <sroertgen@gmail.com>
2023-01-31 10:33:18 +0100

    add file name to error output

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ do
   echo "Processing $f ..."
   # take action on each file. $f store current file name
   scripts/validate-skos.sh -f "$f" -s skos.shacl.ttl -l both 2>/dev/null
-  if test $? -eq 1; then echo "Error: This should be valid, but is invalid"; error=true; fi;
+  if test $? -eq 1; then echo "Error: $f should be valid, but is invalid"; error=true; fi;
 done
 
 # for files in tests/invalid output should be received
@@ -17,7 +17,7 @@ do
   echo "Processing $f ..."
   # validate with script and log error messages to dev null
   scripts/validate-skos.sh -f "$f" -s skos.shacl.ttl -l both 2>/dev/null
-  if test $? -eq 0; then echo "Error: This should be invalid, but is valid"; error=true; fi
+  if test $? -eq 0; then echo "Error: $f should be invalid, but is valid"; error=true; fi
 done
 
 if test "$error" = true; 
